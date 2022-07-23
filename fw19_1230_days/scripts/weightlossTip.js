014578d6686f55f196541dc58fb559c8f2b95faa
Sanjan Yadav <sanjanyadav060@gmail.com>
2022-07-23 23:01:34 +0530

    project done

--- fw19_1230_days/scripts/weightlossTip.js
+++
@@ -1,2 +0,0 @@
-import navbar from "../component/navbar.js";
-document.getElementById("navbar").innerHTML=navbar();
--- scripts/login.js
+++ scripts/login.js
@@ -26,3 +26,7 @@ function myfunction(){
     })
   
 }
+
+function signup(){
+    window.location.href = "signup.html"
+   }
